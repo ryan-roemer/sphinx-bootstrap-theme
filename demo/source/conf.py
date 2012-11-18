b04c0b415bed8e87e07304da6d830bc70f69129d
Ryan Roemer <ryan@loose-bits.com>
2012-11-18 06:15:16 -0500

    Make work for demo

--- demo/source/conf.py
+++ demo/source/conf.py
@@ -105,7 +105,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path() + []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
