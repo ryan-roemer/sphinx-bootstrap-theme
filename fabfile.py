427d2b299699e10a23e3f1bcb0fc6c619f119366
Ryan Roemer <ryan@loose-bits.com>
2012-11-17 21:53:06 -0500

    Setup

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,78 @@ import os
 import json
 import urllib2
 
+from contextlib import contextmanager
+
 from fabric.api import local, lcd, abort
 from fabric.decorators import task
 
 
+BUILD_DIRS = (
+    "dist",
+    "sphinx_bootstrap_theme.egg-info",
+)
+
+SDIST_RST_FILES = (
+    "README.rst",
+    "HISTORY.rst",
+)
+SDIST_TXT_FILES = [os.path.splitext(x)[0] + ".txt" for x in SDIST_RST_FILES]
+
+
+###############################################################################
+# PyPi
+###############################################################################
+@task
+def clean():
+    """Clean build files."""
+    for build_dir in list(BUILD_DIRS) + [DOC_OUTPUT, DEV_DB_DIR]:
+        local("rm -rf %s" % build_dir)
+
+
+@contextmanager
+def _dist_wrapper():
+    """Add temporary distribution build files (and then clean up)."""
+    try:
+        # Copy select *.rst files to *.txt for build.
+        for rst_file, txt_file in zip(SDIST_RST_FILES, SDIST_TXT_FILES):
+            local("cp %s %s" % (rst_file, txt_file))
+
+        # Perform action.
+        yield
+    finally:
+        # Clean up temp *.txt files.
+        for rst_file in SDIST_TXT_FILES:
+            local("rm -f %s" % rst_file, capture=False)
+
+
+@task
+def sdist():
+    """Package into distribution."""
+    with _dist_wrapper():
+        local("python setup.py sdist", capture=False)
+
+
+@task
+def pypi_register():
+    """Register and prep user for PyPi upload.
+
+    .. note:: May need to weak ~/.pypirc file per issue:
+        http://stackoverflow.com/questions/1569315
+    """
+    with _dist_wrapper():
+        local("python setup.py register", capture=False)
+
+
+@task
+def pypi_upload():
+    """Upload package."""
+    with _dist_wrapper():
+        local("python setup.py sdist upload", capture=False)
+
+
+###############################################################################
+# GitHub
+###############################################################################
 class Request(urllib2.Request):
     """Request with method support."""
 
@@ -155,7 +223,7 @@ def bundle(tag=False):
 
 
 @task
-def downloads():
+def gh_downloads():
     """Verify GitHub downloads."""
     print("Downloads:")
     for download in GitHub().downloads():
@@ -163,7 +231,7 @@ def downloads():
 
 
 @task
-def upload(tag=False):
+def gh_upload(tag=False):
     """Upload new zip files.
 
     @param tag  Use git tag instead of hash?
---
+++ setup.py
@@ -0,0 +1,66 @@
+"""Sphinx Bootstrap Theme package."""
+import os
+from setuptools import setup, find_packages
+
+from sphinx_bootstrap_theme import __version__
+
+###############################################################################
+# Environment and Packages.
+###############################################################################
+# Don't copy Mac OS X resource forks on tar/gzip.
+os.environ['COPYFILE_DISABLE'] = "true"
+
+# Packages.
+MOD_NAME = "sphinx_bootstrap_theme"
+PKGS = [x for x in find_packages() if x.split('.')[0] == MOD_NAME]
+
+
+###############################################################################
+# Helpers.
+###############################################################################
+def read_file(name):
+    """Read file name (without extension) to string."""
+    cur_path = os.path.dirname(__file__)
+    exts = ('txt', 'rst')
+    for ext in exts:
+        path = os.path.join(cur_path, '.'.join((name, ext)))
+        if os.path.exists(path):
+            with open(path, 'rb') as file_obj:
+                return file_obj.read()
+
+    return ''
+
+
+###############################################################################
+# Setup.
+###############################################################################
+setup(
+    name="sphinx-bootstrap-theme",
+    version=__version__,
+    description="Sphinx Bootstrap Theme.",
+    long_description=read_file("README"),
+    url="http://ryan-roemer.github.com/sphinx-bootstrap-theme",
+
+    author="Ryan Roemer",
+    author_email="ryan@loose-bits.com",
+
+    classifiers=[
+        "Development Status :: 5 - Stable",
+        "Environment :: Web Environment",
+        "Framework :: Sphinx",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ],
+
+    install_requires=[
+        "distribute",
+    ],
+
+    packages=PKGS,
+    include_package_data=True,
+)
