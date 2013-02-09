0b2fa66947fcc687f962be83e601b37e5c57d23e
Ryan Roemer <ryan@loose-bits.com>
2013-02-09 18:04:58 -0500

    Version bump

--- fabfile.py
+++ fabfile.py
@@ -22,11 +22,11 @@ BUILD_DIRS = (
     "sphinx_bootstrap_theme.egg-info",
 )
 
-SDIST_RST_FILES = (
-    "README.rst",
-    "HISTORY.rst",
-)
-SDIST_TXT_FILES = [os.path.splitext(x)[0] + ".txt" for x in SDIST_RST_FILES]
+# SDIST_RST_FILES = (
+#     "README.rst",
+#     "HISTORY.rst",
+# )
+# SDIST_TXT_FILES = [os.path.splitext(x)[0] + ".txt" for x in SDIST_RST_FILES]
 
 
 ###############################################################################
@@ -49,27 +49,26 @@ def demo():
 ###############################################################################
 # PyPI
 ###############################################################################
-@contextmanager
-def _dist_wrapper():
-    """Add temporary distribution build files (and then clean up)."""
-    try:
-        # Copy select *.rst files to *.txt for build.
-        for rst_file, txt_file in zip(SDIST_RST_FILES, SDIST_TXT_FILES):
-            local("cp %s %s" % (rst_file, txt_file))
+# @contextmanager
+# def _dist_wrapper():
+#     """Add temporary distribution build files (and then clean up)."""
+#     try:
+#         # Copy select *.rst files to *.txt for build.
+#         for rst_file, txt_file in zip(SDIST_RST_FILES, SDIST_TXT_FILES):
+#             local("cp %s %s" % (rst_file, txt_file))
 
-        # Perform action.
-        yield
-    finally:
-        # Clean up temp *.txt files.
-        for rst_file in SDIST_TXT_FILES:
-            local("rm -f %s" % rst_file, capture=False)
+#         # Perform action.
+#         yield
+#     finally:
+#         # Clean up temp *.txt files.
+#         for rst_file in SDIST_TXT_FILES:
+#             local("rm -f %s" % rst_file, capture=False)
 
 
 @task
 def sdist():
     """Package into distribution."""
-    with _dist_wrapper():
-        local("python setup.py sdist", capture=False)
+    local("python setup.py sdist", capture=False)
 
 
 @task
@@ -79,15 +78,13 @@ def pypi_register():
     .. note:: May need to weak ~/.pypirc file per issue:
         http://stackoverflow.com/questions/1569315
     """
-    with _dist_wrapper():
-        local("python setup.py register", capture=False)
+    local("python setup.py register", capture=False)
 
 
 @task
 def pypi_upload():
     """Upload package."""
-    with _dist_wrapper():
-        local("python setup.py sdist upload", capture=False)
+    local("python setup.py sdist upload", capture=False)
 
 
 ###############################################################################
