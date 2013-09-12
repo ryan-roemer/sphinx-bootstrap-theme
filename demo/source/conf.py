06285cbf57057d2475859f7888e41f4ed6ebfd9c
Russell Sim <russell.sim@gmail.com>
2013-09-12 22:28:59 +1000

    Refresh sidebar for bootsrap 3.

--- demo/source/conf.py
+++ demo/source/conf.py
@@ -194,7 +194,7 @@ html_static_path = ["_static"]
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
-html_sidebars = {'sidebar': ['localtoc.html']}
+html_sidebars = {'sidebar': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
