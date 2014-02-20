3e1065094c91dc6bf6e56e202f262872d042d16d
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2014-02-20 08:32:42 +0100

    source link position defaults to footer now
    Signed-off-by: Torbjörn Klatt <opensource@torbjoern-klatt.de>

--- demo/source/conf.py
+++ demo/source/conf.py
@@ -150,7 +150,7 @@ html_theme_options = {
 
     # Location of link to source.
     # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "nav",
+    'source_link_position': "footer",
 
     # Bootswatch (http://bootswatch.com/) theme.
     #
