9801ffc944088f1782cfb2335b9ea7cd0278e7ff
karelv <karel.vanroye@gmail.com>
2018-01-11 21:21:30 +0100

    added setup-function
    In order to fix #172

--- sphinx_bootstrap_theme/__init__.py
+++ sphinx_bootstrap_theme/__init__.py
@@ -10,3 +10,9 @@ def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     return [cur_dir]
+
+def setup(app):
+    # add_html_theme is new in Sphinx 1.6+
+    if hasattr(app, 'add_html_theme'):
+        theme_path = os.path.abspath(os.path.dirname(__file__))
+        app.add_html_theme('bootstrap', os.path.join(theme_path, "bootstrap"))
