a7c2dff700ce54b248bb8696a128aa9adac595d9
karelv <karel.vanroye@gmail.com>
2018-01-22 13:38:54 +0100

    fix the 0.6.2 patch
    obvious I think

--- sphinx_bootstrap_theme/__init__.py
+++ sphinx_bootstrap_theme/__init__.py
@@ -15,5 +15,5 @@ def setup(app):
     """Setup."""
     # add_html_theme is new in Sphinx 1.6+
     if hasattr(app, 'add_html_theme'):
-        theme_path = get_html_theme_path()
+        theme_path = get_html_theme_path()[0]
         app.add_html_theme('bootstrap', os.path.join(theme_path, 'bootstrap'))
